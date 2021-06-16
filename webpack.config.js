895d5095e734ffdfc83a479e9a91bc1da09f8cb3
SergeMaikere <serge.maikere@gmail.com>
2021-06-16 15:17:27 +0200

    adding path to webconfig

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,24 @@
+import * as path from 'path';
+import { fileURLToPath } from 'url';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export default {
-  entry: "./index.js",
-  output: {
-    filename: "./bundle.js",
-    publicPath: ''
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        test: /\.html$/i,
-        loader: 'html-loader'
-      },
-      {
-        test: /\.css$/i,
-        loader: 'css-loader'
-      }
-    ]
-  }	
+    entry: "./index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, 'dist')
+    },
+    mode: "development",
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader'
+            },
+            {
+                test: /\.css$/i,
+                loader: 'css-loader'
+            }
+        ]
+    }	
 };
